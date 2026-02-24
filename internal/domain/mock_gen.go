13ebdbfb4d5951138fb10b13d2715189d67a97ba
Cleiton Souza <cleitonmarx@hotmail.com>
2026-02-23 22:07:08 -0800

    refactor: update Execute method signature in UnitOfWork interface to include context parameter

--- internal/domain/mock_gen.go
+++ internal/domain/mock_gen.go
@@ -3139,7 +3139,7 @@ func (_c *MockUnitOfWork_ConversationSummary_Call) RunAndReturn(run func() Conve
 }
 
 // Execute provides a mock function for the type MockUnitOfWork
-func (_mock *MockUnitOfWork) Execute(ctx context.Context, fn func(uow UnitOfWork) error) error {
+func (_mock *MockUnitOfWork) Execute(ctx context.Context, fn func(ctx context.Context, uow UnitOfWork) error) error {
 	ret := _mock.Called(ctx, fn)
 
 	if len(ret) == 0 {
@@ -3147,7 +3147,7 @@ func (_mock *MockUnitOfWork) Execute(ctx context.Context, fn func(uow UnitOfWork
 	}
 
 	var r0 error
-	if returnFunc, ok := ret.Get(0).(func(context.Context, func(uow UnitOfWork) error) error); ok {
+	if returnFunc, ok := ret.Get(0).(func(context.Context, func(ctx context.Context, uow UnitOfWork) error) error); ok {
 		r0 = returnFunc(ctx, fn)
 	} else {
 		r0 = ret.Error(0)
@@ -3162,20 +3162,20 @@ type MockUnitOfWork_Execute_Call struct {
 
 // Execute is a helper method to define mock.On call
 //   - ctx context.Context
-//   - fn func(uow UnitOfWork) error
+//   - fn func(ctx context.Context, uow UnitOfWork) error
 func (_e *MockUnitOfWork_Expecter) Execute(ctx interface{}, fn interface{}) *MockUnitOfWork_Execute_Call {
 	return &MockUnitOfWork_Execute_Call{Call: _e.mock.On("Execute", ctx, fn)}
 }
 
-func (_c *MockUnitOfWork_Execute_Call) Run(run func(ctx context.Context, fn func(uow UnitOfWork) error)) *MockUnitOfWork_Execute_Call {
+func (_c *MockUnitOfWork_Execute_Call) Run(run func(ctx context.Context, fn func(ctx context.Context, uow UnitOfWork) error)) *MockUnitOfWork_Execute_Call {
 	_c.Call.Run(func(args mock.Arguments) {
 		var arg0 context.Context
 		if args[0] != nil {
 			arg0 = args[0].(context.Context)
 		}
-		var arg1 func(uow UnitOfWork) error
+		var arg1 func(ctx context.Context, uow UnitOfWork) error
 		if args[1] != nil {
-			arg1 = args[1].(func(uow UnitOfWork) error)
+			arg1 = args[1].(func(ctx context.Context, uow UnitOfWork) error)
 		}
 		run(
 			arg0,
@@ -3190,7 +3190,7 @@ func (_c *MockUnitOfWork_Execute_Call) Return(err error) *MockUnitOfWork_Execute
 	return _c
 }
 
-func (_c *MockUnitOfWork_Execute_Call) RunAndReturn(run func(ctx context.Context, fn func(uow UnitOfWork) error) error) *MockUnitOfWork_Execute_Call {
+func (_c *MockUnitOfWork_Execute_Call) RunAndReturn(run func(ctx context.Context, fn func(ctx context.Context, uow UnitOfWork) error) error) *MockUnitOfWork_Execute_Call {
 	_c.Call.Return(run)
 	return _c
 }

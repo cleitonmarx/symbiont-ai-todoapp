8861165f1b2087e906b31f7aa0c96c4176d25078
Cleiton Souza <cleitonmarx@hotmail.com>
2026-03-07 12:58:19 -0800

    refactor: Move Status type and validation logic to todo.go from status.go

--- internal/domain/todo/todo.go
+++ internal/domain/todo/todo.go
@@ -7,6 +7,24 @@ import (
 	"github.com/google/uuid"
 )
 
+// Status represents the status of a todo item.
+type Status string
+
+const (
+	// Status_OPEN indicates that the todo item is open and not yet completed.
+	Status_OPEN Status = "OPEN"
+	// Status_DONE indicates that the todo item has been completed.
+	Status_DONE Status = "DONE"
+)
+
+// Validate checks if the Status is valid.
+func (s Status) Validate() error {
+	if s != Status_OPEN && s != Status_DONE {
+		return core.NewValidationErr("status must be either OPEN or DONE")
+	}
+	return nil
+}
+
 // Todo represents a todo item in the system.
 type Todo struct {
 	ID        uuid.UUID
